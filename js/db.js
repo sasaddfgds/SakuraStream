4df8fe583aa72fe401fe810133f6e9f3b4010541
sasaddfgds <fhhj6442@gmail.com>
2026-02-08 14:11:56 +0100

    Added Naruto and Berserk

--- js/db.js
+++ js/db.js
@@ -25,5 +25,32 @@ const animeData = [
         rating: "8.2",
         status: "finished",
         link: "https://kodik.info/find-player?shikimoriID=48661"
-    }
+    },
+    {
+        id: "naruto-1",
+        title: "Наруто (1 сезон)",
+        poster: "assets/img/Naruto.jpg",
+        episodes: "220/ 220",
+        rating: "8.4",
+        status: "finished",
+        link: "https://kodik.info/find-player?shikimoriID=20"
+    },
+    {
+        id: "naruto-shippuden",
+        title: "Наруто: Ураганные хроники",
+        poster: "assets/img/Naruto.jpg",
+        episodes: "500/500",
+        rating: "8.7",
+        status: "finished",
+        link: "https://kodik.info/find-player?shikimoriID=1735"
+    },
+    {
+        id: "berserk-1997",
+        title: "Берсерк (1997)",
+        poster: "assets/img/Berserk.jpg",
+        episodes: "25/25",
+        rating: "8.7",
+        status: "finished",
+        link: "https://kodik.info/find-player?shikimoriID=33"
+    },
 ];
